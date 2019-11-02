da13c504d0a48f1a0e09ab58d1f844d80c81301a
Simon Buckner <simon@onebyte.net>
2019-11-02 11:37:02 +0000

    Define the configuration file entries
    Covers most of the necessary settings.
    
    TODO:
    Mandatory groups to add to each user account
    Remote desktop groups and selection strategies
    
    Remtoe desktop groups will probably need to be a manual selection as
    there are lots of possible strategies.

---
+++ isElevated.ps1
@@ -0,0 +1,8 @@
+# File to be dot sourced by main scripts
+# Checks to see if the scripted is running in a elvated session
+# return true if it is, false if it is now
+
+
+
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
