27971dcc4f39f709273173e3a6f0eb7ff95530a2
SimonBuckner <simonbuckner@hotmail.com>
2019-11-13 08:09:39 +0000

    Change from pascal to camel case in config file
    Done for purley cosmetic purposes. Other languages make that change for
    you whereas PowerShell doesn't. Pascal case makes for some horrible
    looking code. For example.
    
    Pascal: $config.email_domain
    Camel: $config.EmailDomain
    
    TODO Load config into an objet with vetting and pascal/camel conversion

--- ps/loadConfig.ps1
+++ ps/loadConfig.ps1
@@ -1,6 +1,11 @@
 # File to be dot sourced by main scripts
 # Load config from JSON
 
+<# TODO
+    Return a concrete object which validates the config file.
+    Also allows for pascal case in the JSON file which is more common
+    whilst retaining camel case in the script which is more common.
+#>
 function loadConfig($Path) {
 
     if (Test-Path $Path) {
